8da598ef471e5883c0025f1482d65b16a1aaafb0
johnathanvidu <johnathan.v@qualisystems.com>
2025-10-24 12:30:43 +0300

    modfiy outputs.sh

--- scripts/outputs.sh
+++ scripts/outputs.sh
@@ -2,5 +2,5 @@ git config --global user.email "johnathan.v@quali.com"
 git config --global user.name "johnathanvidu"
 cd $ENVIRONMENTS_PATH/scripts
 chmod +x ./init.sh
-./init.sh {{ .inputs.app_name }}-dev {{ .inputs.app_name }} engine_version={{ .inputs.engine_version }} skip_final_version={{ .inputs.skip_final_version }}
+./init.sh $1 $2 $3 $4
 export COMMIT=$(git rev-parse HEAD)
