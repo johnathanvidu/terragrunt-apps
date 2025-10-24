4d1abfec7669c659ad9ba9bcd004d6fbbddf2b49
johnathanvidu <johnathan.v@qualisystems.com>
2025-10-24 13:00:14 +0300

    testing outputs

--- scripts/outputs.sh
+++ scripts/outputs.sh
@@ -1,6 +1,1 @@
-git config --global user.email "johnathan.v@quali.com"
-git config --global user.name "johnathanvidu"
-cd $ENVIRONMENTS_PATH/scripts
-chmod +x ./init.sh
-./init.sh $1 $2 $3 $4
-export COMMIT=$(git rev-parse HEAD)
+export COMMIT=vido
